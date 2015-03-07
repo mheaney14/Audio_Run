4317d2a8b331b4eb5f4f742cd5f3a5bccd90d8ad
Bdog94 <berniemayer94@gmail.com>
2015-03-07 12:43:47 -0700

    Got the menu working

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class ManagerScript : MonoBehaviour {
 
 	public void changeScene(string sceneName) {
 		Application.LoadLevel (sceneName);
 		}
+
+	public void update() {
+		}
+
+	private void FixedUpdate() {
+
+	}
+
+
 }
---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	public void pause() {
+		//store the current location of the player.
+
+		//store the current location in the song
+
+		//
+		}
+
+}
