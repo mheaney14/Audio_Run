7487551596b3cb4706512aba54c2e87bc29c9b86
Mike Heaney <mheaney14@gmail.com>
2015-03-07 12:45:03 -0700

    commit n shit

--- Assets/Scripts/CameraFollowRun.cs
+++ Assets/Scripts/CameraFollowRun.cs
@@ -5,13 +5,16 @@ public class CameraFollowRun : MonoBehaviour {
 
 	public float CameraOffsetX = 15;
 	public float CameraOffsetY = 6;
-
+	public static float oldPositionX = 0;
+	public static float oldPositionY = 0;
 	public Transform player;
 	// Use this for initialization
 
 	
 	// Update is called once per frame
 	void Update () {
+		oldPositionX = player.position.x + CameraOffsetX;
+		oldPositionY = player.position.y + CameraOffsetY;
 		transform.position = new Vector3 (player.position.x + CameraOffsetX, player.position.y + CameraOffsetY, -10);
 		//transform.position = new Vector3 (0, player.position.y + CameraOffsetY, -10);
 	}
