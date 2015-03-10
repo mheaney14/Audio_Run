bf2f3ead64a7ffd37d3e45fead74d264bd7ee4c8
slenderq <justinquaintance@gmail.com>
2015-03-10 09:30:19 -0600

    File explorer working on android!

--- Assets/Scripts/MusicGrabber.js
+++ Assets/Scripts/MusicGrabber.js
@@ -4,12 +4,17 @@
     var myClip : WWW;
     var myPath : String;
     var opened : int;
+    private var message = "";
+	private var alpha = 1.0;
     
     function Start()
     {
+
     	//Time.timeScale =0;
     	opened = 0;	
-    	UniFileBrowser.use.OpenFileWindow (OpenFolder);
+    	UniFileBrowser.use.OpenFileWindow (OpenFile);
+
+
     	
 	    //myPath = "/mnt/sdcard/music";
 	   
@@ -25,18 +30,26 @@
 
     }
     
+    function OnGUI () {
+	GUI.color.a = alpha;
+	GUI.Label (Rect(100, 275, 500, 1000), message);
+	GUI.color.a = 1.0;
+}
+    
     function loadMusic(){
     	
     	MusicFolder = new System.IO.DirectoryInfo(myPath);
 	    myClip = new WWW("file:///" + MusicFolder.GetFiles()[0].FullName);
 	    audio.clip = myClip.GetAudioClip(false, false);
 	    opened = 1;
+	    message = "Opened!";
 	    
     }
     
-	function OpenFolder (pathToFile : String) {
+	function OpenFile (pathToFile : String) {
 		Debug.Log(pathToFile);
 		myPath = pathToFile;
+		message = "You selected file: " + pathToFile;
 		loadMusic();
 	}
     
