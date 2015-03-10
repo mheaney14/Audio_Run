c0d12aa3a644b434d7275cb015e59c6db7027705
slenderq <justinquaintance@gmail.com>
2015-03-10 00:06:58 -0600

    Limited file explorer functionality
    I added the visualizer as well

--- Assets/Scripts/MusicGrabber.js
+++ Assets/Scripts/MusicGrabber.js
@@ -3,28 +3,45 @@
     var MusicFolder : System.IO.DirectoryInfo;
     var myClip : WWW;
     var myPath : String;
+    var opened : int;
     
     function Start()
     {
-    
-    	UniFileBrowser.use.OpenFileWindow (OpenFile)
+    	//Time.timeScale =0;
+    	opened = 0;	
+    	UniFileBrowser.use.OpenFileWindow (OpenFolder);
     	
 	    //myPath = "/mnt/sdcard/music";
-	    //MusicFolder = new System.IO.DirectoryInfo(myPath);
-	    myClip = new WWW("file:///" + myPath);
-	    audio.clip = myClip.GetAudioClip(false, false);
+	   
+	    //Time.timeScale =1;
     }
-    function Update()
+        function Update()
     {
-	    if (!audio.isPlaying && audio.clip.isReadyToPlay){
-	    audio.Play();
+    	if (opened == 1){
+    			    if (!audio.isPlaying && audio.clip.isReadyToPlay){
+	   					 audio.Play();
+    				}
+    	}
+
     }
     
-    
-    
-    
+    function loadMusic(){
+    	
+    	MusicFolder = new System.IO.DirectoryInfo(myPath);
+	    myClip = new WWW("file:///" + MusicFolder.GetFiles()[0].FullName);
+	    audio.clip = myClip.GetAudioClip(false, false);
+	    opened = 1;
+	    
     }
     
-    void OpenFile (string pathToFile) {
+	function OpenFolder (pathToFile : String) {
+		Debug.Log(pathToFile);
 		myPath = pathToFile;
+		loadMusic();
 	}
+    
+    
+    
+    
+    
+    
---
+++ Assets/Scripts/MusicLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicLoader : MonoBehaviour {
+
+	string myPath;
+
+	// Use this for initialization
+	void Start () {
+		//myPath = "/mnt/sdcard/music";
+
+		UniFileBrowser.use.OpenFileWindow (OpenFile);
+		WWW myClip = new WWW("file:///" + myPath);
+		audio.clip = myClip.GetAudioClip(false, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (!audio.isPlaying && audio.clip.isReadyToPlay) {
+						audio.Play ();
+				}
+		}
+	void OpenFile (string pathToFile) {
+		myPath = pathToFile;
+	}
+
+}
