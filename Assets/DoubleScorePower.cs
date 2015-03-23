3e21265337ceb09b807bbffdc596a2babd989f4d
Mike Heaney <mheaney14@gmail.com>
2015-03-22 18:34:27 -0600

    Started work on implementing score and score mods
    Created a few scripts to track and adjust player score

---
+++ Assets/DoubleScorePower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class DoubleScorePower : MonoBehaviour {
+	[SerializeField] private int doubleTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") {
+			GameManager.doubleScore = true;
+//			GetComponent(MeshRenderer).enabled = false;
+			//Thread.Sleep(doubleTime);
+			//GameManager.doubleScore = false;
+//			Destroy(GameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class GameManager : MonoBehaviour {
+	[SerializeField]  public static double score;
+	public static bool doubleScore;
+	[SerializeField]  private int scoreMulti;
+	[SerializeField]  private double increment;
+	[SerializeField]  private string difficulty;
+	private string scoreString;
+
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		if (difficulty == "Easy") {
+						scoreMulti = scoreMulti / 2;
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doubleScore == true) {
+						score = (score + increment) * scoreMulti + 500;
+				} 
+		else {
+						score = (score + increment) * scoreMulti;
+						scoreString = score.ToString ();
+						Thread.Sleep(100);
+				}
+		}
+	void OnGUI (){
+			//var col = GUI.color;
+			//col.a = alpha;
+			//GUI.color = col;
+			GUI.Label (new Rect(100, 275, 500, 1000), scoreString);
+			//col.a = 1.0f;
+			//GUI.color = col;
+		}
+	}
+
