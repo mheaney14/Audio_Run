7c0deb33177dd34321b4d5702841068d9cc78939
Mike Heaney <mheaney14@gmail.com>
2015-03-31 13:30:22 -0600

    Continued work on Powerups

--- Assets/DoubleScorePower.cs
+++ Assets/DoubleScorePower.cs
@@ -11,12 +11,12 @@ public class DoubleScorePower : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.tag == "Player") {
+		//if (other.tag == "Player") {
 			GameManager.doubleScore = true;
 //			GetComponent(MeshRenderer).enabled = false;
 			//Thread.Sleep(doubleTime);
 			//GameManager.doubleScore = false;
 //			Destroy(GameObject);
-		}
+		//}
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using System.Threading;
 
 public class GameManager : MonoBehaviour {
 	[SerializeField]  public static double score;
-	public static bool doubleScore;
+    public static bool doubleScore;
 	[SerializeField]  private int scoreMulti;
 	[SerializeField]  private double increment;
 	[SerializeField]  private string difficulty;
@@ -21,14 +21,16 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (doubleScore == true) {
+	if (doubleScore == true) {
 						score = (score + increment) * scoreMulti + 500;
+						scoreString = score.ToString ();
 				} 
 		else {
 						score = (score + increment) * scoreMulti;
 						scoreString = score.ToString ();
-						Thread.Sleep(100);
+						//Thread.Sleep(100);
 				}
+		//GUI.Label (new Rect(100, 275, 500, 1000), scoreString);
 		}
 	void OnGUI (){
 			//var col = GUI.color;
