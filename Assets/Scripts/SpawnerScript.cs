0577d46b8e6619d1ec7d271cceb520c3c0ba0eef
slenderq <justinquaintance@gmail.com>
2015-04-01 10:28:00 -0600

    Did the graphics for the powerups and made them spawn.

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -22,11 +22,11 @@ public class SpawnerScript : MonoBehaviour {
 	void Spawn(){
 	//	if (CameraFollowRun.oldPositionX != CameraFollowRun.currentPositionX & CameraFollowRun.oldPositionY != CameraFollowRun.currentPositionY) {
 						Invoke ("Spawn", Random.Range (spawnMin, spawnMax)); 
-						float deltapos = Random.Range (-delta, delta);
-						Vector3 position = new Vector3 (this.transform.position.x, deltapos);
+						//float deltapos = Random.Range (-delta, delta);
+		Vector3 position = new Vector3 (this.transform.position.x, this.transform.position.y+delta);
 						Instantiate (obj [Random.Range (0, obj.Length)], position, Quaternion.identity);
-						transform.Translate (Vector3.up * deltapos);
-						Thread.Sleep (4);
+						//transform.Translate (Vector3.up * deltapos);
+						//Thread.Sleep (4);
 			//	}
 	}
 }
