037337a86e63d38eff4b758d69f0260aafe2517a
slenderq <justinquaintance@gmail.com>
2015-03-31 22:21:05 -0600

    Messed up Mikes commits in fixing stuff, all fixed now

---
+++ Assets/Scripts/DoubleScorePower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class DoubleScorePower : MonoBehaviour {
+	[SerializeField] private float doubleTime;
+	// Use this for initialization	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.tag == "Player") {
+			GameManager.doubleScore = true;
+			Destroy(gameObject);
+				}
+			//GameManager.doubleScore = true;
+//			GetComponent(MeshRenderer).enabled = false;
+			//Thread.Sleep(doubleTime);
+			//GameManager.doubleScore = false;
+//			Destroy(GameObject);
+		//}
+	}
+}
