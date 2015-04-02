77b9ba77608634db414cce51e0fb0303906901cf
Mike Heaney <mheaney14@gmail.com>
2015-04-01 22:43:04 -0600

    Modified Bad Blocks to affect Score

--- Assets/Scripts/BadBlockScript.cs
+++ Assets/Scripts/BadBlockScript.cs
@@ -5,16 +5,17 @@ public class BadBlockScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player") {
-			Application.LoadLevel(0);
-			return;		
+			GameManager.score = GameManager.score - 1000;
+			Destroy (gameObject);
+			//Application.LoadLevel(0);
 		}
 		
-		if (other.gameObject.transform.parent) {
+	/*	if (other.gameObject.transform.parent) {
 			Destroy (other.gameObject.transform.parent.gameObject);
 		} 
 		else {
 			Destroy (other.gameObject);
 		}
-		
+	*/	
 	}
 }
